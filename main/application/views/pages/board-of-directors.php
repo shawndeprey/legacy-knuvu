7a1a9e7643000d0c70cf4e74e21ed95ebc6c5957
shawndeprey <shawndeprey@gmail.com>
2019-02-10 17:21:31 -0700

    Make site changes for silva

--- main/application/views/pages/board-of-directors.php
+++ main/application/views/pages/board-of-directors.php
@@ -18,47 +18,57 @@
 
   <div class="copy-body">
     <div class="inner-copy">
+
       <p>
-        <strong>Mike Silva:</strong> Mike Silva is a Mobile & Emerging Technologies Strategy Consultant to startups, 
-        Carriers, NEPs, and F100s. He is founder and inventor of KnuVu, a heatmapping strategy tool used for Innovation, 
-        Corporate Development, & Strategy. Mike grew up with the Silicon Valley and after an early career in accounting, 
-        finance, and IT, earned a Masters of Science in Finance with honors. Mike was the CFO of VPC, an early Speech 
-        Recognition pioneer where he met Jabra and later went to work with Jabra as VP Strategy. Mike left Jabra to fund 
-        and lead to Video over IP companies to exits before founding brauning, LLC, a mobile and emerging technologies 
-        strategy boutique with 20 consultants with offices in NY, SF, SEA, & San Diego whose clients included all the 
-        Carriers, Qualcomm, Microsoft, Ericsson, Alcatal-Lucent and many mobile upstarts such as Openwave. Mike’s approach 
-        to strategy has always layered a foundation of Michael Porter’s Competitive Strategy with deep domain expertise and 
+        <strong>Mike Silva:</strong> Mike Silva is a Mobile & Emerging Technologies Strategy Consultant to startups,
+        Carriers, NEPs, and F100s. He is founder and inventor of KnuVu, a heatmapping strategy tool used for Innovation,
+        Corporate Development, & Strategy. Mike grew up with the Silicon Valley and after an early career in accounting,
+        finance, and IT, earned a Masters of Science in Finance with honors. Mike was the CFO of VPC, an early Speech
+        Recognition pioneer where he met Jabra and later went to work with Jabra as VP Strategy. Mike left Jabra to fund
+        and lead to Video over IP companies to exits before founding brauning, LLC, a mobile and emerging technologies
+        strategy boutique with 20 consultants with offices in NY, SF, SEA, & San Diego whose clients included all the
+        Carriers, Qualcomm, Microsoft, Ericsson, Alcatal-Lucent and many mobile upstarts such as Openwave. Mike’s approach
+        to strategy has always layered a foundation of Michael Porter’s Competitive Strategy with deep domain expertise and
         hands-on front-line experience and execution:
       </p>
       <ul>
         <li>At VPC he programmed the first drive-through application for a McDonald’s trial;</li>
         <li>
-          At Jabra he borrowed technicians’ PCs after hours and trialed 40+ soundboards to invent the first universal 
+          At Jabra he borrowed technicians’ PCs after hours and trialed 40+ soundboards to invent the first universal
           microphone and audio jack adapter (and was later banned from the lab);
         </li>
         <li>
-          Mike was the first to send Video streams to EU and Asia (he also pulled them back) by setting up a global ATM 
+          Mike was the first to send Video streams to EU and Asia (he also pulled them back) by setting up a global ATM
           network for the Grammys in 1996 which subsequently became the prototype for CDNs;
         </li>
         <li>
-          Mike wasn’t just an investor and CEO, he became the “Red Adair” of webcasting and was contracted by Ericsson to 
-          fly to Iceland with Keiko (Free Willy) to engineer the first video webcast from a plane so concerned students 
+          Mike wasn’t just an investor and CEO, he became the “Red Adair” of webcasting and was contracted by Ericsson to
+          fly to Iceland with Keiko (Free Willy) to engineer the first video webcast from a plane so concerned students
           across the globe could track his progress.
         </li>
       </ul>
       <p>
-        Mike has always been held up as an innovative strategist and business/corporate development executive but where Mike 
-        really differentiates from others is in his ability, built by diving in and getting hands-on where needed, to go broader 
+        Mike has always been held up as an innovative strategist and business/corporate development executive but where Mike
+        really differentiates from others is in his ability, built by diving in and getting hands-on where needed, to go broader
         and deeper within technological segments to identify opportunities and value creation.
       </p>
       <p>
-        Mike was recruited out of brauning to head up wireless data marketing strategy for AT&T and subsequently to Qpass to spin 
-        up business development in preparation for its acquisition by Amdocs. Mike helped spin UIEvolution out of Japanese gaming 
-        giant SquareEnix and funded the transaction by revenue from AT&T and other Carrier deals, growing the company from startup 
-        to over $30MM in 2012. Mike took two years to focus on non-profit, working with NPOs to do more by adopting SaaS technologies 
-        and running the Mobile Giving Foundation, which allows users to text donations to NPOs. Mike founded KnuVu after leaving the 
-        Mobile Giving Foundation and has been focused on leveraging KnuVu for strategy and Corporate Development as a sell-side 
+        Mike was recruited out of brauning to head up wireless data marketing strategy for AT&T and subsequently to Qpass to spin
+        up business development in preparation for its acquisition by Amdocs. Mike helped spin UIEvolution out of Japanese gaming
+        giant SquareEnix and funded the transaction by revenue from AT&T and other Carrier deals, growing the company from startup
+        to over $30MM. Mike took two years to focus on non-profit, working with NPOs to do more by adopting SaaS technologies and
+        running the Mobile Giving Foundation, which allows users to text donations to NPOs. Mike founded KnuVu after leaving the
+        Mobile Giving Foundation and has been focused on leveraging KnuVu for strategy and Corporate Development as a sell-side
         consultant, advisor, investor, and operator in emerging technology companies.
+      </p>
+    </div>
+
+    <div class="inner-copy">
+      <p>
+        <br />
+        <strong>Bodhi Silva:</strong> Bodhi Silva has joined KnuVu focused on technology transfer and licensing, Bodhi became interested
+        in transactions after immersing himself in gaming, social, communications, and mobile computing technologies. Bodhi is simultaneously
+        pursuing a CS degree.
       </p>
     </div>
   </div>
